5bc7477b5dea22799bd59e0e403be1fae8576505
rauxw <hardikraut909@gmail.com>
2025-03-16 21:56:11 +0530

    Added Routes v1

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { userRouter } = require("./routes/users");
 const { coursesRouter } = require("./routes/courses");
+const { adminRouter } = require("./routes/admin");
 const app = express();
 
 app.use(express.json());
 
-app.use("/user", userRouter);
-app.use("/course", coursesRouter);
+app.use("api/v1/user", userRouter);
+app.use("api/v1/admin", adminRouter);
+app.use("api/v1/course", coursesRouter);
 
 app.listen(3001, () => {
   console.log(`Server is running on port 3001`);
